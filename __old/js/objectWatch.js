e60f85168c8cd313b301db2f9832daf689380061
spat-ne-hochu <spat.ne.hochu@gmail.com>
2015-08-03 02:29:36 +0300

    initial demo

--- __old/js/objectWatch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var object = {
-    a : 3,
-    b : 4,
-    c : 'string'
-};
-
-console.log(Object.keys(object).length);
--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,80 +1,18 @@
 'use strict';
 
 module.exports = function () {
-    var uglify = {
-        devBuild           : {},
-        angularServices    : {
-            options : {
-                sourceMap : true
-            },
-            files   : [
-                {
-                    src  : [
-                        src + 'angular/app.js',
-                        src + 'angular/directives/**/*.js',
-                        src + 'angular/filters/**/*.js',
-                        src + 'angular/i18n/**/*.js',
-                        src + 'angular/services/**/*.js',
-                        src + 'angular/smsAccept/**/*.js'
-                    ],
-                    dest : dist + 'angular/services.min.js'
-                }
-            ]
-        },
-        angularControllers : {
-            options : {
-                sourceMap : true
-            },
-            files   : [
-                {
-                    src  : [
-                        src + 'angular/controllers/user/settings/*.js'
-                    ],
-                    dest : dist + 'angular/controllers/user__settings.min.js'
-                },
-                {
-                    src  : [
-                        src + 'angular/controllers/campaigns/platforms/*.js'
-                    ],
-                    dest : dist + 'angular/controllers/campaigns__platforms.min.js'
-                },
-                {
-                    src  : [
-                        src + 'angular/controllers/campaigns/posts/*.js'
-                    ],
-                    dest : dist + 'angular/controllers/campaigns__posts.min.js'
-                }
-            ]
+    return {
+        devBuild : {
+            src  : [
+                'vendor/jquery/dist/jquery.min.js',
+                'vendor/esprima/esprima.js',
+                'js/src/reactor/reactor-core.js',
+                'js/src/reactor/reactor-debuger.js',
+                'js/src/reactor/reactor-parser.js',
+                'js/src/bindings/bind.js',
+                'js/src/controllers/ExampleController.js'
+            ],
+            dest : 'js/site/build.js'
         }
     };
-
-    uglify.sharedComponents = {
-        options : {
-            sourceMap : true
-        },
-        files   : [
-            {
-                src  : config.js.paths.sharedComponents.srcDir + '/**/*.js',
-                dest : config.js.paths.sharedComponents.tmpDir + 'services.min.js'
-            }
-        ]
-    };
-
-    config.js.maps.getActionScripts().forEach(
-        function (controller) {
-            uglify[controller.name] = {
-                options : {
-                    sourceMap : true
-                },
-                files   : [
-                    {
-                        src  : controller.src,
-                        dest : controller.dest
-                    }
-                ]
-            };
-        }
-    );
-
-    return uglify;
 };
